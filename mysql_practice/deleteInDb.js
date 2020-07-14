5a06e3d1b0cdb5b9e0d9abe06a5b211922f64d2a
unknown <ashikiqbal53@gmail.com>
2020-07-14 21:26:32 +0600

    updated

--- mysql_practice/deleteInDb.js
+++ mysql_practice/deleteInDb.js
@@ -1,10 +1,12 @@
 var conn = require('./connection');
 
+
+let line = 2;
 conn.connect((err) => {
 	
 	if(err) throw err;
 	
-	var sql = "DELETE FROM identity WHERE roll = 37";
+	var sql = "DELETE FROM identity WHERE roll = " + line; //dyanmic value
 
 	conn.query(sql, (err, result)=> {
 		if(err) throw err;
